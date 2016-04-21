efd954abc9ee0ccba9aa9c51be125d79cb55ce22
James Dabbs <jamesdabbs@gmail.com>
2016-04-21 09:52:02 -0400

    Make an API call

--- js/controllers/channels.js
+++ js/controllers/channels.js
@@ -5,9 +5,28 @@
     .module("slactory")
     .controller("ChannelsController", ChannelsController);
 
-  function ChannelsController() {
+  ChannelsController.$inject = ["$http"];
+
+  function ChannelsController($http) {
+    var token = window.slackToken; // TODO: get from a factory / localStorage
+
+    $http({
+      method: "POST",
+      url: "https://slack.com/api/channels.list?token=" + token
+    }).then(onSuccess, onFailure);
+
     this.list = [
       { name: "Ours", id: "C0LJ6NG5S" }
     ]
+  }
+
+  // Helper functions
+
+  function onSuccess(response) {
+    console.log('success', response);
+  }
+
+  function onFailure(response) {
+    console.log('failure', response);
   }
 })();
